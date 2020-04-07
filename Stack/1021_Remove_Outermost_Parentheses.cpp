42981f442b4e24c8f9fe58374450c770dcad618b
Rahul Dubey <37729273+LogiCule@users.noreply.github.com>
2020-04-07 11:35:02 +0530

    Update 1021_Remove_Outermost_Parentheses.cpp

--- Stack/1021_Remove_Outermost_Parentheses.cpp
+++ Stack/1021_Remove_Outermost_Parentheses.cpp
@@ -12,13 +12,11 @@ public:
                 {count;}
                 else
                 {
-                    cout<<S[top];
                     word=word+S[top];
                 }
             }
             if(S[top]=='(')
             {
-                
                 count++;
             }
             else if(S[top]==')')
@@ -34,4 +32,8 @@ public:
 };
 /*
 Problem:https://leetcode.com/problems/remove-outermost-parentheses/
+Explaination->this problem requires u to remove the outermost bracket so what we do is keep track of how many brackets are currently open
+so we go to the stack if there is only one element in the stack that means one bracket is there so we do not count it
+and do not asdd it to the string
+and if the bracket is being closed we also check if closing this bracket will make the stack empty so we do not add this bracket to the result string
 */
